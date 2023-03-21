5215d9c937655d66598dae5be4845e73dbb2d1d0
lcc206 <1402080954@qq.com>
2023-03-21 23:33:56 +0800

    complete base code

--- src/app.ts
+++ src/app.ts
@@ -2,14 +2,103 @@ import express from 'express';
 import fs from 'fs'
 import path from 'path'
 const app = express();
-const port: number = 8000;
 
-const folderPath = "./";
+function getFilePath(file: string): string {
+    return path.join(__dirname, file);
+}
 
-let array: string[] =  fs.readdirSync(folderPath).map(fileName => {
-    return path.join(folderPath, fileName);
+// Initialize
+const config = JSON.parse(fs.readFileSync(getFilePath("./config.json"), 'utf-8'));
+
+const port: number = config.port as number ?? 8000;
+
+const dirMap: Map<string, string> = new Map<string, string>();
+for (let dir of config.dirMap) {
+    dirMap.set(dir.dirId, dir.path);
+}
+
+app.get('/browser', (req, res) => {
+    let list: Array<string> = new Array<string>();
+    for (let dir of dirMap) {
+        list.push(dir[0]);
+    }
+    res.json(list);
 });
 
-array.forEach(item => {
-    console.log(item);
+app.get('/browser/:dir', (req, res) => {
+    let relativePath: string;
+    relativePath = req.query.relativepath as string ?? '';
+
+    let dir = req.params.dir;
+    if (!dirMap.has(dir)) {
+        res.sendStatus(404);
+    }
+    let dirPath: string = dirMap.get(dir) as string;
+    dirPath = path.join(dirPath, relativePath);
+
+    // 获取所有文件
+    fs.readdir(dirPath, (err, files) => {
+        if (err) {
+            res.sendStatus(404);
+            console.log(err.message);
+            return;
+        }
+        else {
+            let dirList = new Array<string>();
+            let fileList = new Array<string>();
+            files.forEach(file => {
+                try {
+                    let stat: fs.Stats;
+                    stat = fs.statSync(path.join(dirPath, file));
+                    if (stat.isDirectory()) {
+                        dirList.push(file);
+                    }
+                    else {
+                        fileList.push(file);
+                    }
+                }
+                catch (err) {
+                    let error = err as Error;
+                    console.log(error.message);
+                }
+            });
+            let data = {
+                'relativePath': relativePath,
+                'dirList': dirList,
+                'fileList': fileList
+            };
+            res.json(data);
+        }
+    });
+
 });
+
+app.get('/browser/:dir/file/:filename', (req, res) => {
+    let relativePath: string;
+    relativePath = req.query.relativepath as string ?? '';
+
+    let dir = req.params.dir;
+    if (!dirMap.has(dir)) {
+        res.sendStatus(404);
+    }
+
+    let fileName = req.params.filename;
+    if (!fileName) {
+        res.sendStatus(404);
+    }
+
+    let dirPath: string = dirMap.get(dir) as string;
+    dirPath = path.join(dirPath, relativePath);
+
+    let options = {
+        root: dirPath
+    }
+
+    res.sendFile(fileName, options);
+});
+
+
+
+app.listen(port, () => {
+    console.log(`Leaf server is running on port ${port}!`);
+})
